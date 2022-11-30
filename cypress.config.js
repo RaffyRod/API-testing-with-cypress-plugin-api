c46a8fc783da138f53b45f6fa4a0100c2e3a6427
RaffyRod <elraffy3@gmail.com>
2022-11-30 13:02:20 -0400

    cypress-api-plugin, reporter and ci added

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,28 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  video: false,
+  env:{
+    baseUrl: 'https://reqres.in'
+
+  },reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    reportTitle: 'API Testing Report',
+    reportPageTitle: 'API Test Automation Results',
+    reportFilename: 'API Report',
+    embeddedScreenshots: true,
+    charts: true,
+    inline: true,
+    overwrite: false,
+    autoOpen: false,
+    showPending: false,
+    showSkipped: true,
+    timestamp: "longDate",
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
---
+++ cypress/e2e/API/list_users.cy.js
@@ -0,0 +1,9 @@
+describe('List users', () => {
+  it('List users from API', () => {
+    cy.api(Cypress.env('baseUrl') + '/api/users?page=2')
+      .then((response) => {
+        expect(response.status).to.be.equal(200);
+        expect(response.body).not.to.be.null;
+      });
+  })
+})
